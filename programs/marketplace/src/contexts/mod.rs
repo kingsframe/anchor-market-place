c0ba579dea71605148d0a974673b143c44037f40
kai <kaizhangwen@gmail.com>
2025-02-01 13:28:15 -0800

    add init

---
+++ programs/marketplace/src/contexts/mod.rs
@@ -0,0 +1,4 @@
+pub mod init;
+
+
+pub use init::*;
--- programs/marketplace/src/lib.rs
+++ programs/marketplace/src/lib.rs
@@ -2,15 +2,19 @@ use anchor_lang::prelude::*;
 
 declare_id!("7pLE2CQhUnfyNHj4eF6dXkkY4eu3wSs1rMkb2gRHZhs2");
 
+mod state;
+mod errors;
+
+mod contexts;
+use contexts::*;
+use errors::*;
 #[program]
 pub mod marketplace {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Initialize>, name: String, fee: u16) -> Result<()> {
+        ctx.accounts.init(name, fee, &ctx.bumps)?;
+
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
